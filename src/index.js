b91e164c67faf61140e6077e9a73e93808fb8838
unknown <brodny@wp.pl>
2020-09-14 22:25:11 +0200

    Board generated by a loop instead of hardcoding

--- src/index.js
+++ src/index.js
@@ -21,23 +21,20 @@ class Board extends React.Component {
   }
 
   render() {
+    let content = [];
+
+    for (let row = 0; row < 3; row++) {
+      let buttons = [];
+      for (let col = 0; col < 3; col++) {
+        buttons.push(this.renderSquare(row * 3 + col));
+      }
+
+      content.push(<div className="board-row">{buttons}</div>);
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {content}
       </div>
     );
   }
